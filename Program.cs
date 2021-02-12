afe050a7bed61872c4f9349053fa54a274be4232
Celio Camargo Junior <celio.camargo@opus-software.com.br>
2021-02-12 19:00:39 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,13 +7,25 @@ namespace CLIExample
     {
         private static int Main(string[] args)
         {
-            return Parser.Default.ParseArguments(args, new[] { typeof(KafkaOptions), typeof(GrpcOptions) })
+            return Parser.Default.ParseArguments<KafkaOptions, GrpcOptions>(args)
                 .MapResult
                 (
-                    (KafkaOptions opts) => { Console.WriteLine($"kafka: {opts.Topic} : {opts.Consumers} : {opts.Producers}"); return 0; },
-                    (GrpcOptions opts) => { Console.WriteLine($"grpc: {opts.Port}"); return 0; },
+                    (KafkaOptions opts) => KafkaHandler(opts),
+                    (GrpcOptions opts) => GrpcHandler(opts),
                     errs => 1
                 );
+        }
+
+        private static int KafkaHandler(KafkaOptions opts)
+        {
+            Console.WriteLine($"kafka: {opts.Topic} : {opts.Consumers} : {opts.Producers}");
+            return 0;
+        }
+
+        private static int GrpcHandler(GrpcOptions opts)
+        {
+            Console.WriteLine($"grpc: {opts.Port}");
+            return 0;
         }
     }
 }
